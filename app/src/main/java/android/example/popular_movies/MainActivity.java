863519b2878eb52ed8156352fea805e7c7787160
Toto Corvidoni <totocorvidoni@gmail.com>
2020-10-21 11:20:25 -0300

    Add Api Key

--- app/src/main/java/android/example/popular_movies/MainActivity.java
+++ app/src/main/java/android/example/popular_movies/MainActivity.java
@@ -5,14 +5,17 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final int NUM_LIST_ITEMS = 100;
     private PosterAdapter mAdapter;
     private RecyclerView mPosterList;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i("MainActivity", BuildConfig.MovieApiKey);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -24,5 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
         mAdapter = new PosterAdapter(NUM_LIST_ITEMS);
         mPosterList.setAdapter(mAdapter);
+    }
+
+    void makeTopMoviesQuery() {
+
     }
 }
